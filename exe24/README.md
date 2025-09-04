3a7a0b7aa6f12b507bd455b4af740f3d69361e26
apdilahii <apdilahiiicisman@gmail.com>
2025-09-04 10:21:08 +0300

    Completed Exe#24

---
+++ exe24/README.md
@@ -0,0 +1,5 @@
+<h1> This Exercise about Default Parameters</h1>
+
+
+![Exercise 24](https://github.com/azzatosma/dugsiiye-js-exercises/blob/main/assets/jsexe24.jpg)
+
---
+++ exe24/exe24.js
@@ -0,0 +1,5 @@
+// Exercise #24
+function calculateArea(width, height=width) {
+    console.log(`The area of the screen is ${width * height}`);
+}
+calculateArea(10,20);
