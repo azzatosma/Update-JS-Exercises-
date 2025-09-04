c7faaba5d81cf00869ff00e46862eb306123292c
apdilahii <apdilahiiicisman@gmail.com>
2025-09-04 10:24:55 +0300

    Completed Exe#32

---
+++ exe32/README.md
@@ -0,0 +1,5 @@
+<h1> This Exercise about DOM</h1>
+
+
+![Exercise 32](https://github.com/azzatosma/dugsiiye-js-exercises/blob/main/assets/jsexe32.jpg)
+
---
+++ exe32/exe32.js
@@ -0,0 +1,4 @@
+const card = document.querySelectorAll('.student-card');
+console.log(card);
+const header = document.querySelector('#s-card');
+console.log(header);
