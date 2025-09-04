fe861f3fe87cc64fd66840384787f0bacc4650a6
apdilahii <apdilahiiicisman@gmail.com>
2025-09-04 10:19:56 +0300

    Completed Exe#22

---
+++ exe22/README.md
@@ -0,0 +1,5 @@
+<h1> This Exercise about Array Destructuring</h1>
+
+
+![Exercise 22](https://github.com/azzatosma/dugsiiye-js-exercises/blob/main/assets/jsexe22.jpg)
+
---
+++ exe22/exe22.js
@@ -0,0 +1,5 @@
+// Exercise #22
+const colors = ["Red", "Green", "Blue"];
+let [first , second , third] = colors;
+console.log(`The first color ${first} and Third color is ${third}`); // The first color Red and Third color is Blue
+
