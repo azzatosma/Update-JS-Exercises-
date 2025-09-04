86b2993e071d5c9abdb3d9672ed1a053a29d742e
apdilahii <apdilahiiicisman@gmail.com>
2025-09-04 10:20:35 +0300

    Completed Exe#23

---
+++ exe23/README.md
@@ -0,0 +1,5 @@
+<h1> This Exercise about Object Destructuring</h1>
+
+
+![Exercise 23](https://github.com/azzatosma/dugsiiye-js-exercises/blob/main/assets/jsexe23.jpg)
+
---
+++ exe23/exe23.js
@@ -0,0 +1,4 @@
+// Exercise #23
+const cars = {make : "Toyota", model : "Corolla", Year : 2000};
+let {make,model} = cars;
+console.log(`My Car made by ${make} and his model is ${model}`);
