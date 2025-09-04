c3ac4c588d138b863c576cbe09bbf9078da9b34a
apdilahii <apdilahiiicisman@gmail.com>
2025-09-04 10:19:33 +0300

    Completed Exe#21

---
+++ exe21/README.md
@@ -0,0 +1,5 @@
+<h1> This Exercise about reduce method</h1>
+
+
+![Exercise 21](https://github.com/azzatosma/dugsiiye-js-exercises/blob/main/assets/jsexe21.jpg)
+
---
+++ exe21/exe21.js
@@ -0,0 +1,4 @@
+// Exercise #21 
+const number = [1, 2, 3, 4, 5];
+const sum = number.reduce((total, num) => total * num , 1);
+console.log("The answer is : "+sum);
